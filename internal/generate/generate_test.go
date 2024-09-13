1cc82909726f74bb5942f1d4a0034bb0f3377cfc
Akilesh Sethu <asethu19@gmail.com>
2024-09-13 08:53:18 +0800

    formatting

--- internal/generate/generate_test.go
+++ internal/generate/generate_test.go
@@ -18,20 +18,20 @@ func (p profileRepositoryStub) GetDetailsByProfileName(profileName string) ([]da
 
 func TestGenerate(t *testing.T) {
 	testcases := []struct {
-		name        string
-		details     []data.Detail
-		err         error
-		res         string
+		name    string
+		details []data.Detail
+		err     error
+		res     string
 	}{
-        {
-            name: "empty details should return empty string",
-        },
-        {
-            name: "non-empty details should return string with exports and aliases",
-            details: []data.Detail{{Key: "test_env", Value: "test_env_value", DetailType: data.EnvDetail}, {Key: "test_alias", Value: "test_alias_value", DetailType: data.AliasDetail}},
-            res: "export test_env=test_env_value;alias test_alias=test_alias_value;",
-        },
-    }
+		{
+			name: "empty details should return empty string",
+		},
+		{
+			name:    "non-empty details should return string with exports and aliases",
+			details: []data.Detail{{Key: "test_env", Value: "test_env_value", DetailType: data.EnvDetail}, {Key: "test_alias", Value: "test_alias_value", DetailType: data.AliasDetail}},
+			res:     "export test_env=test_env_value;alias test_alias=test_alias_value;",
+		},
+	}
 
 	for _, testcase := range testcases {
 		t.Run(testcase.name, func(t *testing.T) {
